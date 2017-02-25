d843fdbbd5d6140bfa0eb687d99ce65f8f3ffcf1
Dmitry Borisov <borisov.dmitry.a@gmail.com>
2017-02-25 13:18:10 +0300

    public access to example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, unless: -> { devise_controller? }
+  before_action :authenticate_user!, unless: -> { public_acceess? }
 
   def new_session_path(scope)
     new_user_session_path
+  end
+
+  private
+
+  def public_acceess?
+    return true if params[:action].include?('example')
+    devise_controller?
   end
 end
